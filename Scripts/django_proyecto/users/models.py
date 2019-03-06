39689e2f580f7626101f09f8851b67296e1eb7e9
jjsebastianfuertes <jjsebastianfuertes@gmail.com>
2019-03-06 10:52:34 -0500

    Updadte User Profile

--- Scripts/django_proyecto/users/models.py
+++ Scripts/django_proyecto/users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+# resize images
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -8,3 +10,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Perfil'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
